db96b55b2259bd67d553d70ffa4002e518003042
Katawut Chuasiripattana <katawut@kobidigital.com>
2016-09-06 13:56:39 +0700

    Add main menu

--- SchoolWorx/mainMenu.php
+++ SchoolWorx/mainMenu.php
@@ -8,10 +8,22 @@ and open the template in the editor.
     <head>
         <meta charset="UTF-8">
         <title></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     </head>
     <body>
         <?php
         echo "This is the main menu page.";
         ?>
+
+        <ul class="nav nav-pills">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#">Menu 1</a></li>
+            <li><a href="#">Menu 2</a></li>
+            <li><a href="#">Menu 3</a></li>
+        </ul>
+
     </body>
 </html>
