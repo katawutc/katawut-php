d3f29c2a955cfc47d82d89cb7661a0acdd14c885
Katawut Chuasiripattana <katawut@kobidigital.com>
2016-09-07 11:55:22 +0700

    my courses menu

--- SchoolWorx/myCourses.php
+++ SchoolWorx/myCourses.php
@@ -7,7 +7,7 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>SchoolWorx</title>
+        <title>My Courses</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
@@ -27,6 +27,5 @@ and open the template in the editor.
                 </ul>
             </div>
         </nav>
-
     </body>
 </html>
