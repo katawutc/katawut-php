6adb629cce462b46642f0acac9cfc9e28b3f8a14
Katawut Chuasiripattana <katawut@kobidigital.com>
2016-09-07 14:13:23 +0700

    Login form

---
+++ SchoolWorx/homeWork.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php include("navigationBar.html"); ?> 
+        
+        <h1> To do and submit Homework </h1>
+    </body>
+</html>
