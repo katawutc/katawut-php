5b14fba7e36d6fa959cc95c1bbdba14366d2862d
Katawut Chuasiripattana <katawut@kobidigital.com>
2016-09-07 13:42:00 +0700

    Login form

--- SchoolWorx/index.php
+++ SchoolWorx/index.php
@@ -20,7 +20,18 @@ and open the template in the editor.
         ?>
 
         <form action="mainMenu.php" method="post">
-            <input type='submit' name='submit' class='register' />
+            <div class="form-group">
+                <label for="email">Email address:</label>
+                <input type="email" class="form-control" id="email">
+            </div>
+            <div class="form-group">
+                <label for="pwd">Password:</label>
+                <input type="password" class="form-control" id="pwd">
+            </div>
+            <div class="checkbox">
+                <label><input type="checkbox"> Remember me</label>
+            </div>
+            <button type="submit" class="btn btn-default">Submit</button>
         </form>
     </body>
 </html>
