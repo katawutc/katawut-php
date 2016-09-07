92a09967fb8ff5f7576f1e685d9408e6a3d08bae
Katawut Chuasiripattana <katawut@kobidigital.com>
2016-09-07 14:22:25 +0700

    Login form

--- SchoolWorx/homeWork.php
+++ SchoolWorx/homeWork.php
@@ -8,10 +8,14 @@ and open the template in the editor.
     <head>
         <meta charset="UTF-8">
         <title></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     </head>
     <body>
         <?php include("navigationBar.html"); ?> 
-        
+
         <h1> To do and submit Homework </h1>
     </body>
 </html>
