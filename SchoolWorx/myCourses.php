7ef46bd7c1cd0f8e0a99ff2d0dff35fbc921ef17
Katawut Chuasiripattana <katawut@kobidigital.com>
2016-09-07 12:26:59 +0700

    Have navigation Bar organized by include

--- SchoolWorx/myCourses.php
+++ SchoolWorx/myCourses.php
@@ -14,18 +14,6 @@ and open the template in the editor.
         <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     </head>
     <body>
-        <nav class="navbar navbar-default">
-            <div class="container-fluid">
-                <div class="navbar-header">
-                    <a class="navbar-brand" href="#">SchoolWorx</a>
-                </div>
-                <ul class="nav navbar-nav">
-                    <li class="active"><a href="mainMenu.php">Home</a></li>
-                    <li><a href="myCourses.php">My Courses</a></li>
-                    <li><a href="#">Page 2</a></li>
-                    <li><a href="#">Page 3</a></li>
-                </ul>
-            </div>
-        </nav>
+        <?php include("navigationBar.html"); ?>
     </body>
 </html>
